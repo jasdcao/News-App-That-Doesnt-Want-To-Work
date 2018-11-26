640738e9ec15e67fa0fc9296de924e896a632d02
jasdcao <42665111+jasdcao@users.noreply.github.com>
2018-11-25 23:18:54 -0800

    Add files via upload
    implements lines in NewsItemDao

--- app/src/main/java/com/example/rkjc/news_app_2/NewsItemDao.java
+++ app/src/main/java/com/example/rkjc/news_app_2/NewsItemDao.java
@@ -1,6 +1,20 @@
 package com.example.rkjc.news_app_2;
+import android.arch.lifecycle.LiveData;
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
 
-import NewsItemDao;
+import java.util.List;
 
-public class NewsItemDao implements NewsItemDao {
+@Dao
+public interface NewsItemDao {
+
+    @Query("SELECT * from news_item ORDER BY author ASC")
+    LiveData<List<NewsItem>> loadAllNewsItems();
+
+    @Insert
+    void insert(List<NewsItem> item);
+
+    @Query("DELETE FROM news_item")
+    void deleteAll();
 }
