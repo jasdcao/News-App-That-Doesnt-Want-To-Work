d21bd633ee8321338864be5005dc48fb78a43c8c
Jas <jas.dcao@protonmail.com>
2018-11-28 22:56:32 -0800

    NewsItemDataBase

---
+++ app/src/main/java/com/example/rkjc/news_app_2/NewsItemDataBase.java
@@ -0,0 +1,12 @@
+package com.example.rkjc.news_app_2;
+import android.arch.persistence.db.SupportSQLiteDatabase;
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.support.annotation.NonNull;
+
+public abstract class NewsItemDataBase extends NewsDataBase {
+
+}
